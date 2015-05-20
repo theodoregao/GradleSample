2ba12f1ffaf8cb7ea10ff37bb9665215ee15b3ed
Shun Gao <theodore.gao.1986@gmail.com>
2015-05-19 23:33:57 -0700

    first commit

---
+++ sdk-library-release/src/androidTest/java/shun/gao/sg/sdk_library_release/ApplicationTest.java
@@ -0,0 +1,13 @@
+package shun.gao.sg.sdk_library_release;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ sdk-library/src/main/java/shun/gao/sg/sdk_library/SdkLibrary.java
@@ -0,0 +1,15 @@
+package shun.gao.sg.sdk_library;
+
+/**
+ * SDK Library
+ */
+public class SdkLibrary {
+
+    /**
+     * do nothing function
+     * @return nothing
+     */
+    public static String doNothing() {
+        return "do nothing";
+    }
+}
